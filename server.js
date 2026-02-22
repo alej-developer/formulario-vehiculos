66c389396de7706232f475fd554500a32f09074f
alej-developer <alejandropm32@gmail.com>
2026-02-22 12:51:18 +0100

    Finalizar proyecto: depuración completada

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ app.use(express.static(path.join(__dirname)));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+const PORT = 3000; 
+app.listen(PORT, () => {
+    console.log(`Servidor en puerto${PORT}`);
+}); 
